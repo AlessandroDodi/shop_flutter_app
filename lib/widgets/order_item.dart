004f517cf1b29061f1bd431104bc031f274acf23
Alessandro Dodi <dodialessandro3@gmail.com>
2023-01-17 18:49:27 +0100

    Expanded in orders

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:intl/intl.dart';
 import 'package:shop_flutter_app/providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
   const OrderItem(this.order, {super.key});
 
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -13,13 +20,38 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
-            subtitle: Text(DateFormat('dd MM yyyy').toString()),
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(DateFormat('dd/MM/yyyy')
+                .format(widget.order.dateTime)
+                .toString()),
             trailing: IconButton(
-              icon: const Icon(Icons.abc),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_more : Icons.expand_less),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
           ),
+          if (_expanded)
+            SizedBox(
+              height: min(widget.order.products.length * 20 + 100, 180),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView(children: [
+                  ...widget.order.products.map((e) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              '${e.title}',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Divider(),
+                            Text('${e.quantity}x \$${e.price}')
+                          ])),
+                ]),
+              ),
+            )
         ],
       ),
     );

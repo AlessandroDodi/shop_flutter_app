3443c2fe12263cd010af556c9361ba0ef40ed945
Alessandro Dodi <dodialessandro3@gmail.com>
2023-01-18 14:38:23 +0100

    Edit product screen

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,10 +10,33 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+  final _imageUrlFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Edit product page')),
+      appBar: AppBar(title: const Text('Edit product page')),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
@@ -33,8 +56,52 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   decoration: const InputDecoration(
                     labelText: 'Price',
                   ),
+                  onFieldSubmitted: (_) => FocusScope.of(context)
+                      .requestFocus(_descriptionFocusNode),
                   focusNode: _priceFocusNode,
                   textInputAction: TextInputAction.next,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  maxLines: 3,
+                  decoration: const InputDecoration(
+                    labelText: 'Description',
+                  ),
+                  focusNode: _descriptionFocusNode,
+                ),
+                Row(
+                  children: [
+                    ClipRRect(
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        margin: const EdgeInsets.only(
+                          top: 20,
+                          right: 10,
+                        ),
+                        // decoration: BoxDecoration(
+                        //     border: Border.all(width: 1, color: Colors.grey),
+                        //     borderRadius: BorderRadius.circular(20)),
+                        child: _imageUrlController.text.isEmpty
+                            ? const Center(child: Text('Enter a URL'))
+                            : FittedBox(
+                                child: Image.network(_imageUrlController.text,
+                                    fit: BoxFit.cover),
+                              ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Image URL'),
+                          keyboardType: TextInputType.url,
+                          textInputAction: TextInputAction.done,
+                          controller: _imageUrlController,
+                          onEditingComplete: () {
+                            setState(() {});
+                          }),
+                    ),
+                  ],
                 ),
               ],
             ),

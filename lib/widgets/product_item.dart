71f0062dc7f6d6cbc1d27b29283b2ca4e03ee29c
Alessandro Dodi <dodialessandro3@gmail.com>
2023-01-18 11:31:57 +0100

    ADd remove item with snackbar feature

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,8 +9,9 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context, listen: false);
-    final cart = Provider.of<Cart>(context, listen: false);
+    final product = Provider.of<Product>(context);
+    final cart = Provider.of<Cart>(context);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GestureDetector(
@@ -35,6 +36,16 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).colorScheme.secondary,
               onPressed: () {
                 cart.addItem(product.id, product.price, product.title);
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: const Text('added item to cart'),
+                    duration: const Duration(seconds: 2),
+                    action: SnackBarAction(
+                        label: 'Remove item',
+                        onPressed: () => cart.removeSingleItem(product.id)),
+                  ),
+                );
               },
             ),
             title: Text(

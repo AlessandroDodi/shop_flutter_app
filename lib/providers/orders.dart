bca1b3f13920b8d28c52134a0268c6a754c63544
Alessandro Dodi <dodialessandro3@gmail.com>
2023-01-19 17:00:37 +0100

    Add futurebuilder

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -46,7 +46,7 @@ class Orders with ChangeNotifier {
               .toList(),
           dateTime: DateTime.parse(value['dateTime'])));
     });
-    _orders = loadedOrders;
+    _orders = loadedOrders.reversed.toList();
     notifyListeners();
   }
 
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,48 +4,34 @@ import 'package:shop_flutter_app/providers/orders.dart';
 import 'package:shop_flutter_app/widgets/app_drawer.dart';
 import 'package:shop_flutter_app/widgets/order_item.dart' as o;
 
-class OrderScreen extends StatefulWidget {
+class OrderScreen extends StatelessWidget {
   static String routeName = '/product';
 
   const OrderScreen({super.key});
 
   @override
-  State<OrderScreen> createState() => _OrderScreenState();
-}
-
-class _OrderScreenState extends State<OrderScreen> {
-  var _isLoading = false;
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final ordersData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your orders'),
       ),
       drawer: const AppDrawer(),
-      body: _isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemBuilder: ((context, index) =>
-                  o.OrderItem(ordersData.orders[index])),
-              itemCount: ordersData.orders.length,
-            ),
+      body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (context, snapshot) {
+            return snapshot.connectionState == ConnectionState.waiting
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Consumer<Orders>(
+                    builder: (context, value, child) => ListView.builder(
+                      itemBuilder: ((context, index) =>
+                          o.OrderItem(value.orders[index])),
+                      itemCount: value.orders.length,
+                    ),
+                  );
+          }),
     );
   }
 }
